3eb2408cbcde88e15f3d36e4e13a5a173487994d
julian <juliancanderson@gmail.com>
2022-04-03 13:05:00 +0800

    feat: make contract ERC721 and ERC1155 Receiver

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,6 +1,61 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.0;
 
-contract Contract {
+import {ERC721TokenReceiver} from "solmate/tokens/ERC721.sol";
+import {ERC1155TokenReceiver} from "solmate/tokens/ERC1155.sol";
+
+contract Contract is ERC721TokenReceiver, ERC1155TokenReceiver {
     string public greet = "Hello World!";
+
+    //Interface IERC721/IERC1155
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external override returns (bytes4) {
+        return
+            bytes4(
+                keccak256("onERC721Received(address,address,uint256,bytes)")
+            );
+    }
+
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external override returns (bytes4) {
+        return
+            bytes4(
+                keccak256(
+                    "onERC1155Received(address,address,uint256,uint256,bytes)"
+                )
+            );
+    }
+
+    function onERC1155BatchReceived(
+        address operator,
+        address from,
+        uint256[] calldata id,
+        uint256[] calldata value,
+        bytes calldata data
+    ) external override returns (bytes4) {
+        return
+            bytes4(
+                keccak256(
+                    "onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"
+                )
+            );
+    }
+
+    function supportsInterface(bytes4 interfaceID)
+        public
+        view
+        virtual
+        returns (bool)
+    {
+        return interfaceID == 0x01ffc9a7 || interfaceID == 0x4e2312e0;
+    }
 }
